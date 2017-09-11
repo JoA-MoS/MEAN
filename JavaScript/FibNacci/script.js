a124ea510d71f3b3bd492ebfbdb7cd72b54c58a8
Justin Dietz <justin.r.dietz@gmail.com>
2017-09-11 14:42:30 -0700

    FibNacci

---
+++ JavaScript/FibNacci/script.js
@@ -0,0 +1,28 @@
+function fib() {
+    // Some variables here
+    var fcount = 0;
+    var ncount = 1;
+    console.log('fib count', fcount);
+    function nacci() {
+        let sum;
+        sum = ncount + fcount;
+        console.log(sum);
+        ncount = fcount;
+        fcount = sum;
+
+    }
+    return nacci
+}
+var fibCounter = fib();
+fibCounter(); // should console.log "1"
+fibCounter(); // should console.log "1"
+fibCounter(); // should console.log "2"
+fibCounter(); // should console.log "3"
+fibCounter(); // should console.log "5"
+fibCounter(); // should console.log "8"
+fibCounter(); // should console.log "1"
+fibCounter(); // should console.log "1"
+fibCounter(); // should console.log "2"
+fibCounter(); // should console.log "3"
+fibCounter(); // should console.log "5"
+fibCounter(); // should console.log "8"
